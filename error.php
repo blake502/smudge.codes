b5a4d38b0b470db273f47273ab9bda035a7d925f
Blake <102875455+blake502@users.noreply.github.com>
2023-05-18 14:21:48 -0400

    Cleaned up error pages, included licenses

---
+++ error.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head><meta charset="UTF-8"/>
+		<title>Smudge Codes</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+        <style>
+body {
+	font-family: 'Roboto Mono', monospace;
+    background-color: #000;
+    color: #ffffff;
+}
+</style>
+	</head>
+	<body>
+        Oh gee, you've ended up somewhere you shouldn't have.
+        </br>
+        Send an email to: <a href="mailto:admin@smudge.codes">admin@smudge.codes</a> with the following error:
+        </br>
+        Error <?php echo $_GET["e"];?> 
+	</body>
+</html>
